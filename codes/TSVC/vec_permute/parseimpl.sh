56455ab6c62de3a0db5f42cdec1b05e16903985a
Will Killian <william.killian@gmail.com>
2014-04-03 22:37:31 -0400

    Improved csv generator -- now account for execution errors

--- codes/TSVC/vec_permute/parseimpl.sh
+++ codes/TSVC/vec_permute/parseimpl.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env bash
 
+pattern="Time(Sec)"
+parsing=0
 (while read line
-do
-    bench=$(echo $line | sed 's/[^_]*_\(.*\)/\1/;s/\.*exe//')
-    read line
-    read line
-    read line
-    set -- $line
-    time=$2
-    chksum=$3
-    echo "$bench,$time,$chksum"
+    do
+    if ! [[ $line = *$pattern* ]] # ignore lines with /pattern/
+    then
+	if [[ -n "$line" ]] # check if line is not empty
+	then
+	    if [[ $parsing -eq 0 ]] # if we haven't read benchmark, read it!
+	    then
+		bench=$(echo $line | sed 's/[^_]*_\(.*\)/\1/;s/\.*exe//')
+		parsing=1
+	    else
+		set -- $line
+		if [[ -z "$3" ]] # we actually read the next benchmark; store it
+		then
+		    bench=$(echo $line | sed 's/[^_]*_\(.*\)/\1/;s/\.*exe//')
+		    parsing=1
+		else # set time and checksum
+		    parsing=0
+		fi
+		# print
+		time=$2
+		chksum=$3
+		echo "$bench,$time,$chksum"
+	    fi
+	fi
+    fi
 done) | sort -n -k2 -t,
