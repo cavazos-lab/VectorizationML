f29d1ced6d9199720c32b6b0f3360ef1192004ff
Will Killian <william.killian@gmail.com>
2014-04-06 10:27:47 -0400

    Updated cgen

--- codes/Polybench/vec_permute/gencode.sh
+++ codes/Polybench/vec_permute/gencode.sh
@@ -1,52 +1,42 @@
-#./runme.sh -g trmm
-#./runme.sh -g floyd-warshall
-#./runme.sh -g durbin
-#./runme.sh -g atax
-#./runme.sh -g bicg
-#./runme.sh -g gemm
-#./runme.sh -g gesummv
-#./runme.sh -g jacobi-1d-imper
-#./runme.sh -g lu
-#./runme.sh -g seidel-2d
-#./runme.sh -g trisolv
-#./runme.sh -g fdtd-apml 3 1
-#./runme.sh -g mvt 2 2
-#./runme.sh -g syr2k 2 3
-#./runme.sh -g syrk 2 3
-#./runme.sh -g 2mm 3 3
-#./runme.sh -g dynprog 3 3
-#./runme.sh -g doitgen 4 1
-#./runme.sh -g cholesky 1 1 2
-#./runme.sh -g jacobi-2d-imper 1 2 2
-#./runme.sh -g covariance 2 2 3
+./runme.sh -g gesummv 2
+./runme.sh -g trisolv 2
+./runme.sh -g trmm 3
+./runme.sh -g gemm 3
+./runme.sh -g seidel-2d 3
+./runme.sh -g floyd-warshall 3
+./runme.sh -g bicg 1 2
+./runme.sh -g jacobi-1d-imper 2 1
+./runme.sh -g fdtd-apml 3 1
+./runme.sh -g lu-bench 2 2
+./runme.sh -g mvt 2 2
+./runme.sh -g doitgen 4 1
+./runme.sh -g syr2k 2 3
+./runme.sh -g syrk 2 3
+./runme.sh -g 2mm 3 3
+./runme.sh -g dynprog 3 3
+./runme.sh -g atax 1 2 1
+./runme.sh -g cholesky 1 1 2
+./runme.sh -g durbin 2 1 1
+./runme.sh -g jacobi-2d-imper 1 2 2
+./runme.sh -g gramschmidt 2 2 1 
+./runme.sh -g gemver 2 2 2 
+./runme.sh -g covariance 2 2 3
+./runme.sh -g fdtd-2d 3 2 2
+./runme.sh -g 3mm 3 3 3
+./runme.sh -g ludcmp 5 2 2 
+./runme.sh -g reg_detect 4 3 2
 
-./runme.sh -rovi trmm 
-./runme.sh -rovi floyd-warshall 
-./runme.sh -rovi durbin
-./runme.sh -rovi atax
-./runme.sh -crovi bicg
-./runme.sh -crovi gemm 
-./runme.sh -crovi gesummv 
-./runme.sh -crovi jacobi-1d-imper 
-./runme.sh -crovi lu
-./runme.sh -crovi seidel-2d
-./runme.sh -crovi trisolv
-./runme.sh -crovi fdtd-apml 
-./runme.sh -crovi mvt 
-./runme.sh -crovi syr2k 
-./runme.sh -crovi syrk
-./runme.sh -crovi 2mm
-./runme.sh -crovi dynprog
-./runme.sh -crovi doitgen 
-./runme.sh -crovi cholesky 
-./runme.sh -crovi jacobi-2d-imper 
-./runme.sh -crovi covariance
+list="gesummv trisolv trmm gemm seidel-2d floyd-warshall bicg jacobi-1d-imper fdtd-apml lu-bench mvt doitgen syr2k syrk 2mm dynprog atax cholesky durbin jacobi-2d-imper gramschmidt gemver covariance fdtd-2d 3mm ludcmp reg_detect"
 
-./runme.sh -gcrovi 3mm 3 3 3
-./runme.sh -gcrovi gemver 2 2 2 2 
-./runme.sh -gcrovi gramschmidt 2 1 2 1 
-./runme.sh -gcrovi ludcmp 3 2 2 2 
+for i in "$list"
+do
+    ./runme.sh -c $i
+done
+
+for i in "$list"
+do
+    ./runme.sh -rovi $i
+done
+
 ./runme.sh -gcrovi correlation 2 2 2 3 
-./runme.sh -gcrovi fdtd-2d 1 1 2 2 2
-./runme.sh -gcrovi reg_detect 1 3 3 1 2
-./runme.sh -gcrovi adi 1 2 1 2 2 1 2 
+./runme.sh -gcrovi adi 3 1 2 2 2 
