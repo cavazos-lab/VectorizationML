6ef8cc76171f61eee428b435997215ade6745762
willkill07 <william.killian@gmail.com>
2014-04-21 16:49:44 -0400

    Added PAPI versions for ML

---
+++ codes/TSVC/vec_permute/gencode.sh
@@ -0,0 +1,174 @@
+# short runs
+while read line
+do
+    ./runme.sh -gc $line
+done <<EOF
+s13110_papi 3
+s3110_papi 2
+s3111_papi 2
+s3112_papi 2
+s3113_papi 2
+s312_papi 2
+s313_papi 2
+s314_papi 2
+s315_papi 2
+s316_papi 2
+s317_papi 2
+s318_papi 2
+s319_papi 2
+s331_papi 2
+s332_papi 2
+s352_papi 2
+EOF
+# s000_papi 2
+# s1111_papi 2
+# s1112_papi 2
+# s1113_papi 2
+# s1115_papi 3
+# s1119_papi 3
+# s111_papi 2
+# s112_papi 2
+# s113_papi 2
+# s114_papi 3
+# s115_papi 3
+# s1161_papi 2
+# s116_papi 2
+# s118_papi 3
+# s119_papi 3
+# s1213_papi 2
+# s121_papi 2
+# s1221_papi 2
+# s122_papi 2
+# s1232_papi 3
+# s123_papi 2
+# s1244_papi 2
+# s124_papi 2
+# s1251_papi 2
+# s125_papi 3
+# s126_papi 3
+# s1279_papi 2
+# s127_papi 2
+# s1281_papi 2
+# s128_papi 2
+# s13110_papi 3
+# s131_papi 2
+# s132_papi 2
+# s1351_papi 1
+# s141_papi 3
+# s1421_papi 2
+# s151_papi 1
+# s152_papi 2
+# s161_papi 2
+# s162_papi 2
+# s171_papi 2
+# s172_papi 2
+# s173_papi 2
+# s174_papi 2
+# s175_papi 2
+# s176_papi 3
+# s2101_papi 2
+# s2111_papi 3
+# s211_papi 2
+# s212_papi 2
+# s2192_papi 3
+# s221_papi 2
+# s222_papi 2
+# s2233_papi 4
+# s2244_papi 2
+# s2251_papi 2
+# s2275_papi 3
+# s231_papi 3
+# s232_papi 3
+# s233_papi 4
+# s235_papi 3
+# s241_papi 2
+# s242_papi 2
+# s243_papi 2
+# s244_papi 2
+# s251_papi 2
+# s252_papi 2
+# s253_papi 2
+# s254_papi 2
+# s255_papi 2
+# s256_papi 3
+# s257_papi 3
+# s258_papi 2
+# s261_papi 2
+# s2710_papi 2
+# s2711_papi 2
+# s2712_papi 2
+# s271_papi 2
+# s272_papi 2
+# s273_papi 2
+# s274_papi 2
+# s275_papi 3
+# s276_papi 2
+# s277_papi 2
+# s278_papi 2
+# s279_papi 2
+# s281_papi 2
+# s291_papi 2
+# s292_papi 2
+# s293_papi 2
+# s3110_papi 2
+# s31111_papi 1
+# s3111_papi 2
+# s3112_papi 2
+# s3113_papi 2
+# s311_papi 2
+# s312_papi 2
+# s313_papi 2
+# s314_papi 2
+# s315_papi 2
+# s316_papi 2
+# s317_papi 2
+# s318_papi 2
+# s319_papi 2
+# s321_papi 2
+# s322_papi 2
+# s323_papi 2
+# s3251_papi 2
+# s331_papi 2
+# s332_papi 2
+# s341_papi 2
+# s342_papi 2
+# s343_papi 2
+# s351_papi 2
+# s352_papi 2
+# s353_papi 2
+# s4112_papi 2
+# s4113_papi 2
+# s4114_papi 2
+# s4115_papi 2
+# s4116_papi 2
+# s4117_papi 2
+# s4121_papi 2
+# s421_papi 2
+# s422_papi 2
+# s423_papi 2
+# s424_papi 2
+# s431_papi 2
+# s441_papi 2
+# s442_papi 2
+# s443_papi 2
+# s451_papi 2
+# s452_papi 2
+# s453_papi 2
+# s471_papi 2
+# s481_papi 2
+# s482_papi 2
+# s491_papi 2
+# vag_papi 2
+# va_papi 2
+# vas_papi 2
+# vbor_papi 2
+# vdotr_papi 2
+# vif_papi 2
+# vpv_papi 2
+# vpvpv_papi 2
+# vpvts_papi 2
+# vpvtv_papi 2
+# vsumr_papi 2
+# vtv_papi 2
+# vtvtv_papi 2
+# EOF
---
+++ codes/TSVC/vec_permute/s115_papi.c
@@ -0,0 +1,290 @@
+#include <common.h>
+#ifdef PAPI
+#include <papi.h>
+#include <math.h>
+#include <stdlib.h>
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+#define LD_CACHE_SIZE 800000
+typedef unsigned int       uint32;
+typedef unsigned long long uint64;
+typedef union
+{       uint64 int64;
+  struct {uint32 lo, hi;} int32;
+} tsc_counter;
+
+
+#endif
+
+
+
+
+#ifdef PAPI
+void test_fail(char *file, int line, char *call, int retval)
+{
+   char buf[128];
+
+
+   memset(buf, '\0', sizeof(buf));
+   if (retval != 0)
+      fprintf(stdout,"%-40s FAILED\nLine # %d\n", file, line);
+   else {
+      fprintf(stdout,"%-40s SKIPPED\n", file);
+      fprintf(stdout,"Line # %d\n", line);
+   }
+   if (retval == PAPI_ESYS) {
+      sprintf(buf, "System error in %s", call);
+      perror(buf);
+   } else if (retval > 0) {
+      fprintf(stdout,"Error: %s\n", call);
+   } else if (retval == 0) {
+      fprintf(stdout,"Error: %s\n", call);
+   } else {
+      char errstring[PAPI_MAX_STR_LEN];
+      PAPI_perror(errstring);
+      fprintf(stdout,"Error in %s: %s\n", call, errstring);
+   }
+   fprintf(stdout,"\n");
+   if ( PAPI_is_initialized() ) PAPI_shutdown();
+   exit(1);
+}
+#endif
+
+
+
+
+  int retval;
+  int EventSet = PAPI_NULL;
+  long_long values[1];
+  long_long all_values[32];
+  char descr[PAPI_MAX_STR_LEN];
+  PAPI_event_info_t evinfo;
+  long double* cache_cleaner;
+  int cache_iter, cache_loop;
+
+
+  const unsigned int eventlist[] = {
+	PAPI_TOT_CYC,
+	PAPI_L1_DCM,
+	PAPI_L1_ICM,
+	PAPI_L2_DCM,
+	PAPI_L2_ICM,
+	PAPI_L1_TCM,
+	PAPI_L2_TCM,
+	PAPI_L3_TCM,
+	PAPI_L3_LDM,
+	PAPI_TLB_DM,
+	PAPI_TLB_IM,
+	PAPI_TLB_TL,
+	PAPI_L1_LDM,
+	PAPI_L1_STM,
+	PAPI_L2_LDM,
+	PAPI_L2_STM,
+	PAPI_BR_UCN,
+	PAPI_BR_CN,
+	PAPI_BR_TKN,
+	PAPI_BR_NTK,
+	PAPI_BR_MSP,
+	PAPI_BR_PRC,
+	PAPI_TOT_IIS,
+	PAPI_TOT_INS,
+	PAPI_FP_INS,
+	PAPI_LD_INS,
+	PAPI_SR_INS,
+	PAPI_BR_INS,
+	PAPI_RES_STL,
+	PAPI_LST_INS,
+	PAPI_L1_DCH,
+	PAPI_L2_DCH,
+	PAPI_L1_DCA,
+	PAPI_L2_DCA,
+	PAPI_L3_DCA,
+	PAPI_L1_DCR,
+	PAPI_L2_DCR,
+	PAPI_L3_DCR,
+	PAPI_L1_DCW,
+	PAPI_L2_DCW,
+	PAPI_L3_DCW,
+	PAPI_L1_ICH,
+	PAPI_L2_ICH,
+	PAPI_L1_ICA,
+	PAPI_L2_ICA,
+	PAPI_L3_ICA,
+	PAPI_L1_ICR,
+	PAPI_L2_ICR,
+	PAPI_L3_ICR,
+	PAPI_L2_TCH,
+	PAPI_L1_TCA,
+	PAPI_L2_TCA,
+	PAPI_L3_TCA,
+	PAPI_L1_TCR,
+	PAPI_L2_TCR,
+	PAPI_L3_TCR,
+	PAPI_L2_TCW,
+	PAPI_L3_TCW,
+	PAPI_FP_OPS,
+	PAPI_SP_OPS,
+	PAPI_DP_OPS,
+	PAPI_VEC_SP,
+	PAPI_VEC_DP,
+	PAPI_REF_CYC,
+	0
+  };
+
+
+  int evid, eviditer;
+
+
+int s115()
+{
+
+//	linear dependence testing
+//	triangular saxpy loop
+
+	clock_t start_t, end_t, clock_dif; double clock_dif_sec;
+#ifdef PAPI
+  cache_cleaner=(long double*) malloc ((LD_CACHE_SIZE + 4) * sizeof (long double));
+  for (cache_loop = 0; cache_loop < LD_CACHE_SIZE / 64; ++cache_loop)
+    for (cache_iter = 0; cache_iter < LD_CACHE_SIZE; ++cache_iter)
+      cache_cleaner[cache_iter] = M_PI * cache_iter * cache_loop;
+
+
+  if ((retval = PAPI_library_init(PAPI_VER_CURRENT)) != PAPI_VER_CURRENT)
+    test_fail(__FILE__, __LINE__, "PAPI_library_init", retval);
+
+
+  if ((retval = PAPI_create_eventset(&EventSet)) != PAPI_OK)
+    test_fail(__FILE__, __LINE__, "PAPI_create_eventset", retval);
+
+
+  for (evid = 0; eventlist[evid] != 0; evid++)
+    {
+      PAPI_event_code_to_name(eventlist[evid], descr);
+      if (PAPI_add_event(EventSet, eventlist[evid]) != PAPI_OK)
+        continue;
+
+
+      // Clean the cache at each iteration.
+      for (cache_iter = 0; cache_iter < LD_CACHE_SIZE; ++cache_iter)
+      cache_cleaner[cache_iter] *= M_PI + cache_iter;
+
+
+      if (PAPI_get_event_info(eventlist[evid], &evinfo) != PAPI_OK)
+        test_fail(__FILE__, __LINE__, "PAPI_get_event_info", retval);
+
+
+      if ((retval = PAPI_start(EventSet)) != PAPI_OK)
+      test_fail(__FILE__, __LINE__, "PAPI_start", retval);
+
+
+      // Start cycle count.
+#endif
+	start_t = clock();
+
+#pragma autovec permute
+	for (int nl = 0; nl < 1000*(ntimes/LEN2); nl++) {
+#pragma autovec permute
+		for (int j = 0; j < LEN2; j++) {
+#pragma autovec permute
+			for (int i = j+1; i < LEN2; i++) {
+				a[i] -= aa[j][i] * a[j];
+			}
+		}
+		dummy(a, b, c, d, e, aa, bb, cc, 0.);
+	}
+    
+#ifdef PAPI
+      if ((retval = PAPI_read(EventSet, &values[0])) != PAPI_OK)
+        test_fail(__FILE__, __LINE__, "PAPI_read", retval);
+
+
+      if ((retval = PAPI_stop(EventSet,NULL)) != PAPI_OK)
+        test_fail(__FILE__, __LINE__, "PAPI_stop", retval);
+
+
+      all_values[evid] = values[0];
+
+
+      if ((retval = PAPI_remove_event(EventSet, eventlist[evid])) != PAPI_OK)
+        test_fail(__FILE__, __LINE__, "PAPI_remove_event", retval);
+    }
+
+
+  if ((retval = PAPI_destroy_eventset(&EventSet)) != PAPI_OK)
+    test_fail(__FILE__, __LINE__, "PAPI_destroy_eventset", retval);
+
+
+  // Output measure results.
+  printf("8-23");
+  for (eviditer = 0; eviditer < evid; ++eviditer)
+  printf (", %llu", all_values[eviditer]);
+  printf ("\n");
+#else
+#endif
+	end_t = clock(); clock_dif = end_t - start_t;
+	clock_dif_sec = (double) (clock_dif/1000000.0);
+	printf("S115\t %.2f \t\t", clock_dif_sec);;
+	check(1);
+	return 0;
+}
+int main(){
+	int n1 = 1;
+	int n3 = 1;
+	int* ip = (int *) memalign (16, LEN*sizeof(float));
+	float s1,s2;
+	set(ip, &s1, &s2);
+	printf("Loop \t Time(Sec) \t Checksum \n");
+
+	init();
+    s115();
+}
+
+void set(int* ip, float* s1, float* s2){
+	xx = (float*) memalign(16, LEN*sizeof(float));
+	printf("\n");
+	for (int i = 0; i < LEN; i = i+5){
+		ip[i]	= (i+4);
+		ip[i+1] = (i+2);
+		ip[i+2] = (i);
+		ip[i+3] = (i+3);
+		ip[i+4] = (i+1);
+
+	}
+
+	set1d(a, 1.,1);
+	set1d(b, 1.,1);
+	set1d(c, 1.,1);
+	set1d(d, 1.,1);
+	set1d(e, 1.,1);
+	set2d(aa, 0.,-1);
+	set2d(bb, 0.,-1);
+	set2d(cc, 0.,-1);
+
+	for (int i = 0; i < LEN; i++){
+		indx[i] = (i+1) % 4+1;
+	}
+	*s1 = 1.0;
+	*s2 = 2.0;
+
+}
+
+int init()
+{
+	float any=0.;
+	float zero=0.;
+	float half=.5;
+	float one=1.;
+	float two=2.;
+	float small = .000001;
+	int unit =1;
+	int frac=-1;
+	int frac2=-2;
+    
+    set1d(a, one,unit);
+    set2d(aa,small,unit);
+    set2d(bb,small,unit);
+    set2d(cc,small,unit);
+}
